80463f01e87f419a01501d6f1e23f00a4a32c24b
Gameknight12330 <45660710+Gameknight12330@users.noreply.github.com>
2023-04-12 10:57:14 -0400

    Add files via upload

--- java/com/projectmushroom/lavapotions/recipe/ModRecipes.java
+++ java/com/projectmushroom/lavapotions/recipe/ModRecipes.java
@@ -3,6 +3,7 @@ package com.projectmushroom.lavapotions.recipe;
 import com.projectmushroom.lavapotions.LavaPotions;
 
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -10,7 +11,13 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModRecipes {
     public static final DeferredRegister<RecipeSerializer<?>> SERIALIZERS =
             DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, LavaPotions.MOD_ID);
+    
+    public static RecipeType<LavaBrewingCauldronRecipe> LAVA_BREWING_TYPE;
 
     public static final RegistryObject<RecipeSerializer<LavaBrewingCauldronRecipe>> LAVA_BREWING_SERIALIZER =
             SERIALIZERS.register("lava_brewing", () -> LavaBrewingCauldronRecipe.Serializer.INSTANCE);
+    
+    public static void init() { // Run this in FMLCommonSetupEvent
+        LAVA_BREWING_TYPE = RecipeType.register("lavapotions:lava_brewing");
+    }
 }
